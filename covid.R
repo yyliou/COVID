268010eca1db592067c3f481c4a8259ad777326b
Oliver Liou Yu You <128446044+yyliou@users.noreply.github.com>
2023-04-01 14:07:24 +0800

    Update covid.R

--- covid.R
+++ covid.R
@@ -1,52 +1,23 @@
-
 # install.packages(c("httr", "jsonlite"))
 library(httr)
 library(jsonlite)
 
 # function
 covid <- function(code,begin,end){
-  begin <- as.Date(begin)
-  end <- as.Date(end)
-  code <- as.character(code)
-  name <- "world.csv"
-  if(code == "3001" | code == "3002" | code == "3003") {
-      download.file("https://covid-19.nchc.org.tw/api/download/world.csv.gz",name)
-      data <- read.csv(name)
-      data$ID <- NULL
-      unlink(name)
-      for(k in 1:32){
-        if(k < 10){names(data)[k] <- paste0("a0", k)}
-        else{names(data)[k] <- paste0("a" , k)}
-      }
-      if(code == "3001"){data <- data[data$a02 != data$a03,]}
-      if(code == "3002"){data <- data[data$a02 == data$a03 & data$a02 != "全球",]}
-      if(code == "3003"){data <- data[data$a02 == data$a03 & data$a02 == "全球",]}
-    }
-  if(code != "3001" & code != "3002" & code != "3003"){
-    if(code == "4001"){
-      add <- "&limited=TWN"
-    }
-    if(code != "4001"){
-      add <- ""
-    }
-      url <- paste0("https://covid-19.nchc.org.tw/api/covid19",
-                    "?CK=covid-19@nchc.org.tw&querydata=",code,add)
-      res <- GET(url)
-      x <- rawToChar(res$content)
-      Encoding(x) <- "UTF-8"
-      data <- fromJSON(x)
-      data$id <- NULL
-    }
-    if(code == "2002"){data <- data[data$a03 >= begin & data$a03 <= end,]}
-    if(code == "2004"){data <- data[data$a02 >= begin & data$a02 <= end,]}
-    if(code == "3001" | code == "3002" |code == "3003" | code == "4001")
-                      {data <- data[data$a04 >= begin & data$a04 <= end,]}
-    if(code != "2002" & code != "2004" & code != "3001" &
-       code != "3002" & code != "3003" & code != "4001")
-                      {data <- data[data$a01 >= begin & data$a01 <= end,]}
+  url <- paste0(
+    "https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=",code)
+  x <- rawToChar(GET(url)$content)
+  Encoding(x) <- "UTF-8"
+  data <- fromJSON(x)
+  data$id <- NULL
+  if(code == "2004"| code == "4004"){i <- 2}
+  else if(code == "4001" | code == "4050" | code == "4051"){i <- 4}
+  else{i <- 1}
+  var <- paste0("a0",i)
+  data <- data[data[, var] >= begin & data[, var] <= end,]
   data
 }
 
 # example
-data <- covid(code = "4001", begin = "2020-01-01", end = "2022-10-31") 
+data <- covid(code = "2006", begin = "2022-07-22", end = "2022-09-24") 
 
