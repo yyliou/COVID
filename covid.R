75916087602b8d2e8c15cd751c285eb24ac2ba59
Oliver Liou Yu You <128446044+yyliou@users.noreply.github.com>
2023-04-01 10:52:37 +0800

    Update covid.R

--- covid.R
+++ covid.R
@@ -3,20 +3,20 @@
 library(httr)
 library(jsonlite)
 
-# this function must be run!
+# function
 covid <- function(code,begin,end){
   begin <- as.Date(begin)
-  end   <- as.Date(end)
-  code  <- as.character(code)
-  name  <- "world.csv"
+  end <- as.Date(end)
+  code <- as.character(code)
+  name <- "world.csv"
   if(code == "3001" | code == "3002" | code == "3003") {
       download.file("https://covid-19.nchc.org.tw/api/download/world.csv.gz",name)
-      data           <- read.csv(name)
-      data$ID        <- NULL
+      data <- read.csv(name)
+      data$ID <- NULL
       unlink(name)
       for(k in 1:32){
         if(k < 10){names(data)[k] <- paste0("a0", k)}
-        else      {names(data)[k] <- paste0("a" , k)}
+        else{names(data)[k] <- paste0("a" , k)}
       }
       if(code == "3001"){data <- data[data$a02 != data$a03,]}
       if(code == "3002"){data <- data[data$a02 == data$a03 & data$a02 != "全球",]}
@@ -29,13 +29,13 @@ covid <- function(code,begin,end){
     if(code != "4001"){
       add <- ""
     }
-      url         <- paste0("https://covid-19.nchc.org.tw/api/covid19",
-                            "?CK=covid-19@nchc.org.tw&querydata=",code,add)
-      res         <- GET(url)
-      x           <- rawToChar(res$content)
+      url <- paste0("https://covid-19.nchc.org.tw/api/covid19",
+                    "?CK=covid-19@nchc.org.tw&querydata=",code,add)
+      res <- GET(url)
+      x <- rawToChar(res$content)
       Encoding(x) <- "UTF-8"
-      data        <- fromJSON(x)
-      data$id     <- NULL
+      data <- fromJSON(x)
+      data$id <- NULL
     }
     if(code == "2002"){data <- data[data$a03 >= begin & data$a03 <= end,]}
     if(code == "2004"){data <- data[data$a02 >= begin & data$a02 <= end,]}
@@ -48,29 +48,5 @@ covid <- function(code,begin,end){
 }
 
 # example
-data <- covid(code  = "4001",         #  data code from code book
-              begin = "2020-01-01", 
-              end   = "2022-10-31") 
-data <- NULL
-View(data)
-
-
-temp1 <- covid(code  = "3001",         #  data code from code book
-              begin = "2020-03-01", 
-              end   = "2020-03-01") 
-
-temp2 <- covid(code  = "3001",         #  data code from code book
-               begin = "2020-03-31", 
-               end   = "2020-03-31") 
-
-temp3 <- covid(code  = "3001",         #  data code from code book
-               begin = "2020-04-30", 
-               end   = "2020-04-30") 
-
-temp <- rbind(temp1,temp2,temp3)
-use <- temp[temp$a03    == "美國"     | temp$a03 == "日本"|
-               temp$a03 == "中國大陸" | temp$a03 == "韓國"|
-               temp$a03 == "新加坡"   | temp$a03 == "台灣",]
-use <- subset(use, select = c('a11','a12','a03','a04','a05','a08'))
-View(use)
+data <- covid(code = "4001", begin = "2020-01-01", end = "2022-10-31") 
 
